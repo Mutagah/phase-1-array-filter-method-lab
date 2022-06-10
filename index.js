53938f48bbd5223f52647aa18ec204582e52070f
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-06-10 07:32:29 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(myArray,myString)
+{      const matchingData = arrayData => arrayData.toUpperCase() === myString.toUpperCase();
+        return myArray.filter(matchingData)
+
+}
+function fuzzyMatch (myArray, myString)
+{
+        const matchingData = arrayData => arrayData.toLowerCase().charAt(0)  ===  myString.toLowerCase().charAt(0);
+        return myArray.filter(matchingData)
+}
+function matchName(myArray, myString){
+    const matchingData = arrayData => arrayData.name === myString;
+    return myArray.filter(matchingData);
+}
